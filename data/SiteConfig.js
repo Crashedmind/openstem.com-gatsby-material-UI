6f8b9fe2f346c0b18e2d4c7dfba8c1cb9f6589f3
Crashedmind <crashedmind@gmail.com>
2018-12-06 11:57:10 +0000

    added path prefix back in - gatsby requires it

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitleAlt: "OpenSTEMKids Science Technology Engineering Math for kids education", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
   siteUrl: "https://www.openstemkids.com", // Domain of your website without pathPrefix.
-  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  pathPrefix: "/openstem.com-gatsby-material-UI", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
   siteDescription: "Science Technology Engineering Math for kids", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
